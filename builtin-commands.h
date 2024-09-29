bea857e3a436fab9f2d89fe21f65abe2b40dbe9e
maringutierrezd <maringutierrezdavid@gmail.com>
2024-09-29 23:44:08 +0200

    Fixed definition of builtin_func

--- builtin-commands.h
+++ builtin-commands.h
@@ -2,7 +2,7 @@
 #define BUILTIN_COMMANDS_H
 
 extern char* builtin_str[];
-int (*builtin_func[]) (char **);
+extern int (*builtin_func[]) (char **);
 int N_builtins(void);
 
 int dsh_cd(char** args);
