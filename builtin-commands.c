646d6f44aa9dbbd7426c6ec30d74efae5dc3df6f
maringutierrezd <maringutierrezdavid@gmail.com>
2024-09-29 23:56:14 +0200

    Defined 'cd' builtin command

--- builtin-commands.c
+++ builtin-commands.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <unistd.h>
+
 #include "builtin-commands.h"
 
 char* builtin_str[] = {
@@ -12,6 +15,17 @@ int N_builtins(void) {
     return sizeof(builtin_str) / sizeof(char*);
 }
 
-
 int dsh_cd(char** args) {
+    // Check if an argument for the path or folder to change was given
+    if (args[1] == NULL) {
+        fprintf(stderr, "dsh: expected argument to \"cd\"\n");
+    }
+    else {
+        // We change to the folder with syscall chdir
+        int chdir_ret = chdir(args[1]);
+        if (chdir_ret != 0) {
+            perror("dsh");
+        }
+    }
+    return 1;
 }
